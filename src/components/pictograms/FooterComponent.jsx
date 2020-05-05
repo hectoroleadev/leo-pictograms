69d3b0c646440a2ac61d887ddd99db88524bcb18
macako <imacako@gmail.com>
2020-05-05 18:16:01 -0500

    adding media css styles for portrail and landscape

--- src/components/pictograms/FooterComponent.jsx
+++ src/components/pictograms/FooterComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class FooterComponent extends Component {
   render() {
     return (
-      <footer className='footer py-3 px-md-4'>
+      <footer className='footer py-2 px-md-4'>
         <span className='my-0 mr-md-auto text-muted'>
           All rights reserved 2020 @macako
         </span>
