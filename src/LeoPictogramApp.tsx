b63b0689afe22fe4c3dc3eb4b54cc514430a6fee
Hector Olea <imacako@gmail.com>
2023-10-19 13:02:39 -0600

    fixed router for github

--- src/LeoPictogramApp.tsx
+++ src/LeoPictogramApp.tsx
@@ -1,17 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import './css/App.css';
-import { AppRouter } from './router';
+import { router } from './router';
 import { store } from './store/store';
 
 const LeoPictogramApp = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   );
 };
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,16 +1,41 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  Routes,
+  createBrowserRouter,
+  createHashRouter,
+} from 'react-router-dom';
 import { CartoonPage, PictogramPage } from '../pictogram/pages';
-import { URL_BASE } from '../helpers/createPictograms';
+import { URL_BASE_ROUTER, isHashRouter } from '../helpers/createPictograms';
+
+const routes = [
+  {
+    path: `${URL_BASE_ROUTER}/photos`,
+    element: <PictogramPage />,
+  },
+  {
+    path: `${URL_BASE_ROUTER}/cartoons`,
+    element: <CartoonPage />,
+  },
+  {
+    path: `${URL_BASE_ROUTER}/*`,
+    element: <Navigate to={`${URL_BASE_ROUTER}/photos`} replace />,
+  },
+];
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path={`${URL_BASE}/photos`} element={<PictogramPage />} />
-      <Route path={`${URL_BASE}/cartoons`} element={<CartoonPage />} />
+      <Route path={`${URL_BASE_ROUTER}/photos`} element={<PictogramPage />} />
+      <Route path={`${URL_BASE_ROUTER}/cartoons`} element={<CartoonPage />} />
       <Route
-        path={`${URL_BASE}/*`}
-        element={<Navigate to={`${URL_BASE}/photos`} replace />}
+        path={`${URL_BASE_ROUTER}/*`}
+        element={<Navigate to={`${URL_BASE_ROUTER}/photos`} replace />}
       />
     </Routes>
   );
 };
+
+export const router = isHashRouter
+  ? createHashRouter(routes)
+  : createBrowserRouter(routes);
